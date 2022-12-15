8989104c3a104872477cd30f459cdd2db82f9fcb
Rahul Chowdhury <84246818+chowdhuryrahulc@users.noreply.github.com>
2022-12-15 20:28:34 +0530

    still isProgress send data to front-end

--- main.go
+++ main.go
@@ -6,37 +6,39 @@ import (
 	"log"
 	proto "mysqlgolang/gen"
 	"net"
+	"database/sql"
 	"os"
-	"sync"
+
+	// "sync"
 
 	_ "github.com/go-sql-driver/mysql"
 	"google.golang.org/grpc"
 	_ "google.golang.org/grpc/encoding/gzip"
 	glog "google.golang.org/grpc/grpclog"
-	prt "google.golang.org/protobuf/proto" //implements the same stuff as done by "github.com/golang/protobuf/proto"
+	// prt "google.golang.org/protobuf/proto" //implements the same stuff as done by "github.com/golang/protobuf/proto"
 )
 
 /*
 
  */
 
-var grpcLog glog.LoggerV2 
+var grpcLog glog.LoggerV2
 
 func init() {
-	grpcLog = glog.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)	
+	grpcLog = glog.NewLoggerV2(os.Stdout, os.Stdout, os.Stdout)
 }
 
-// This struct gets all the for loop mysql data 
-type MySqlData struct {
-    id int
-    term string
-    defination string
-    favorite string     //! change to bool later
-    image []byte
-}
+// This struct gets all the for loop mysql data
+// type MySqlData struct {
+//     id int
+//     term string
+//     defination string
+//     favorite string     //! change to bool later
+//     image []byte
+// }
 
 type Server struct {
-	MySqlData []*MySqlData
+	MySqlData []*proto.Word
 	proto.UnimplementedBroadcastServer
 }
 
@@ -53,39 +55,106 @@ type Server struct {
 //     return &proto.Vocab{}, nil
 // }
 
-
 func (s *Server) GetData(ctx context.Context, paginate *proto.Paginate) (*proto.Vocab, error) {
-   //! ERROR SOLVED: context is required, otherwise below in registerBroadcastServer will give an error
-   var x []MySqlData
-   x = getSqlData() 
-   return &proto.Vocab{Word: x}, nil
+	//! ERROR SOLVED: context is required, otherwise below in registerBroadcastServer will give an error
+	//    var x []MySqlData
+	fmt.Println("getdata1")				//! not downloading
+	x := getSqlData()
+	fmt.Println("getdata2")
+	fmt.Println(x)
+	return &proto.Vocab{Word: x}, nil 	//! its not Word.Vocab
 
-//todo How do we change []MySqlData to []Word. 
-//todo Means os generted to grpc generated
+// 	//todo How do we change []MySqlData to []Word.
+// 	//todo Means os generted to grpc generated
 
-//    &proto.Vocab{Id: 3, Term: "", Defination: "", Favorite: false, Image: []byte{}}, nil
-//    x, nil
-// PROBLEM: we have to send a list, not 1 term, as done in proto file
+// 	//    &proto.Vocab{Id: 3, Term: "", Defination: "", Favorite: false, Image: []byte{}}, nil
+// 	//    x, nil
+// 	// PROBLEM: we have to send a list, not 1 term, as done in proto file
 }
 
-func main(){
-    //todo Now send all this data via grpc
-    //todo change from incomming msg to broadcast type
-    //todo Favourite change to boolen in proto file
-    
-    var mySqlData []*MySqlData
+func main() {
+	//todo Now send all this data via grpc
+	//todo change from incomming msg to broadcast type
+	//// Favourite change to boolen in proto file
+
+	//! testing sql function
+	// getSqlData() //! error1: cant import from other file, bcoz it shows error
+
+	// fmt.Println("exeo")
+	// x := getSqlData()
+	// fmt.Println("goexeo")
+	// fmt.Println("x value", x)
+
+	//! uncomment below all
+	var mySqlData []*proto.Word
 	server := &Server{mySqlData, proto.UnimplementedBroadcastServer{}} //! Why did we write unimplement...?
 
 	grpcServer := grpc.NewServer()
 	grpcLog.Info("Starting server at port :8080")
 	proto.RegisterBroadcastServer(grpcServer, server)
-    // This server error is due to the server methord
+	// This server error is due to the server methord
 
-    // Start the server.
+	// Start the server.
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("error creating the server %v", err)
 	}
 	grpcServer.Serve(listener)
-    // fmt.Println(x)
+	// fmt.Println(x)
+}
+
+func getSqlData() []*proto.Word {
+	//! Error Solved: panic: runtime error: invalid memory address or nil pointer dereference
+	// SOL: change var r * Rectangle to
+	// 	1) r := &Rectangle{l:10, b:20}
+	// 	2) r := &Rectangle{}
+	// 	3) r := new(Rectangle)
+	// (where Rectangle is a struct with l and b variables and some methords)
+
+	dataRow := &proto.Word{}
+	mysqlAllData := []*proto.Word{}
+
+	// Open up our database connection.
+	// I've set up a database by installing mysql
+	// The database is called vocab
+	db, err := sql.Open("mysql", "root:Chowdhury0511@@tcp(127.0.0.1:3306)/vocab")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished executing
+	defer db.Close()
+
+	// perform a db.Query insert
+	// insert, err := db.Query("INSERT INTO vocabulary VALUES ( 2, 'Sinigalia', 'italy', true, LOAD_FILE('D:/Cdownloads/Flag_of_France.png') )")
+
+	// Get all values
+	getValues, err := db.Query("SELECT * FROM vocabulary")
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// the result object has a method called Next,
+	// which is used to iterate through all returned rows.
+	for getValues.Next() {
+		// The result object provided Scan  method
+		// to read row data, Scan returns error,
+		// if any. Here we read id and name returned.
+		errr := getValues.Scan(&dataRow.Id, &dataRow.Term, &dataRow.Defination, &dataRow.Favorite, &dataRow.Image)
+
+		if errr != nil {
+			panic(err)
+		}
+
+		mysqlAllData = append(mysqlAllData, dataRow)
+	}
+
+	// be careful deferring Queries if you are using transactions
+	defer getValues.Close()
+
+	return mysqlAllData
 }
